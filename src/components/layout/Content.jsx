3d8c868eb9e362408cddd439d6c5ace224aa513e
FelipeUTFPR <fps2887@gmail.com>
2022-02-10 11:11:07 -0300

    53. Aplicando estilo

---
+++ src/components/layout/Content.jsx
@@ -0,0 +1,11 @@
+import './Content.css'
+import React from 'react'
+
+const Content = props =>(
+        <main className='Content'>
+            <h1>Componente App</h1>
+        </main>
+)
+
+
+export default Content
---
+++ src/views/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Menu from '../components/layout/Menu'
+import React from 'react'
+import Content from '../components/layout/Content'
+
+
+const App = props =>(
+        <div className='App'>
+            <Menu/>
+            <Content/>
+        </div>
+)
+
+
+export default App
